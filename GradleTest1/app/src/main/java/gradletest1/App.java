4593b5cc42b1064697b82a1fbcb03f6dedf5e253
sjkang@SNGreen <xeungjin@nate.com>
2022-09-22 15:26:38 +0900

    Merge remote-tracking branch 'origin/master'

--- GradleTest1/app/src/main/java/gradletest1/App.java
+++ GradleTest1/app/src/main/java/gradletest1/App.java
@@ -16,6 +16,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println("test1");
 
         String driver = "oracle.jdbc.driver.OracleDriver";
         String url = "jdbc:oracle:thin:@localhost:1521:xe";
